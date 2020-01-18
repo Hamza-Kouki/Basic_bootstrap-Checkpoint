9502a7f1e010bd896f645c83525984b25224120f
HamzaKouki <hamzakouki33@gmail.com>
2020-01-18 16:46:48 +0100

    newCommit

--- js/main.js
+++
@@ -1,6 +0,0 @@
-$('.icon-list i').on('mouseenter', (e) => {
-    $(e.currentTarget.nextSibling).toggleClass('show')
-})
-$('.icon-list i').on('mouseleave', (e) => {
-    $(e.currentTarget.nextSibling).toggleClass('show')
-})
